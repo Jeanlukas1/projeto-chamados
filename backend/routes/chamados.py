dc00b42e339399092ddd293f430dbb381bbdb71f
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2026-09-12 13:55:08 -0300

    feat: Defini que as funções do Service layer devem retornar apenas dicionários (func() → dict). Adicionei também o try/exception nas funções com excpetions personalizados da camada Exceptions. Também adicionei na camada Router uma nova bibloteca chamada status, que oferece um layout mais clean para os status HTTP. Adicionei também O response_model que garante que apenas os campos definidos no schema sejam enviados na resposta JSON.

--- backend/routes/chamados.py
+++ backend/routes/chamados.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from controllers.chamados import obter_chamados, cadastrar_chamado
-from schemas.chamados import ChamadoRequest
+from schemas.chamados import ChamadoRequest, ChamadoResponse
 
 router = APIRouter()
 
-@router.get("/chamados")
+@router.get("/chamados", status_code=status.HTTP_200_OK, response_model=ChamadoResponse)
 def listar_chamados():
     return obter_chamados()
 
-@router.post("/chamados", status_code=201)
+@router.post("/chamados", status_code=status.HTTP_201_CREATED, response_model=ChamadoResponse)
 def criar_chamado(dados: ChamadoRequest):
     return cadastrar_chamado(dados)
--- backend/services/chamados.py
+++ backend/services/chamados.py
@@ -1,14 +1,20 @@
 from repositories.chamados import chamados, salvar_chamado
 from schemas.chamados import ChamadoRequest
+from exceptions.exceptions import InternalServerError
 
-def listar_chamados():
+def listar_chamados() -> dict:
     return chamados
 
-def criar_chamado(dados: ChamadoRequest):
-    chamado = {
-        "id": str(len(chamados) + 1),
-        "titulo": dados.titulo,
-        "descricao": dados.descricao,
-        "prioridade": dados.prioridade,
-    }
-    return salvar_chamado(chamado)
+def criar_chamado(dados: ChamadoRequest) -> dict:
+    try:
+        chamado = {
+            "id": str(len(chamados) + 1),
+            "titulo": dados.titulo,
+            "descricao": dados.descricao,
+            "prioridade": dados.prioridade,
+        }
+        return salvar_chamado(chamado)
+    except Exception as e:
+        print("error", e)
+        raise InternalServerError()
+    
